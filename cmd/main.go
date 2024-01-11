e93ccaf3846adbb3633ce1f5bf7a3dd941155752
Yael Basurto <ybasurto@bishopfox.com>
2024-01-11 02:13:55 -0600

    Redesign to allow opl do one thing only - logging stuff

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"os"
-	"os/exec"
 	"sync"
 
 	logger "github.com/zkvL/opl/internal/log"
@@ -15,45 +13,36 @@ var wg sync.WaitGroup
 
 func main() {
 	print := flag.String("print", "", "Print log file or folder")
-	runCmd := flag.Bool("runCmd", true, "If false, will only log the command/string that follows.\nYou can use shell env profiles to pass the execution to the shell.")
+	noip := flag.Bool("noip", false, "If true, won't log the IP address of the current machine.")
+	enable := flag.String("enable", "", "Configure shell environment to log commands")
+	disable := flag.String("disable", "", "Disable shell environment from logging commands")
 	flag.Parse()
+
+	if *enable != "" {
+		logger.Enable(*enable)
+		return
+	}
+
+	if *disable != "" {
+		logger.Disable(*disable)
+		return
+	}
 
 	if *print != "" {
 		logger.PrintLogs(*print)
 		return
 	}
 
-	if (len(os.Args) < 2 && *runCmd) || (len(os.Args) < 3 && !*runCmd) {
-		fmt.Printf("Usage: %s [-print <log-file-or-folder>] command [args...]\n", os.Args[0])
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: \t%s [-print <log-file-or-folder>]\n\t%s [-noip] 'actiity-to-log'\n", os.Args[0], os.Args[0])
 		os.Exit(1)
 	}
 
-	wg.Add(2)
-
+	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		logEntry := logger.NewEntry(*runCmd)
+		logEntry := logger.NewEntry(*noip)
 		logger.LogCommand(logEntry)
 	}()
-
-	if *runCmd {
-		go executeCommand(os.Args[1:], *runCmd)
-	} else {
-		go executeCommand(os.Args[3:], *runCmd)
-	}
 	wg.Wait()
-}
-
-func executeCommand(command []string, runCmd bool) {
-	defer wg.Done()
-
-	if runCmd {
-		cmd := exec.Command(command[0], command[1:]...)
-		cmd.Stdin, cmd.Stdout, cmd.Stderr = os.Stdin, os.Stdout, os.Stderr
-
-		if err := cmd.Run(); err != nil {
-			log.Println("[!] Error executing command:", err)
-			os.Exit(1)
-		}
-	}
 }
